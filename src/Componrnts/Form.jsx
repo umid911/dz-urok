83c9e00092f13258e586b49e5a50fe35dca4613c
umid <muzaffarov911gmail.com>
2022-11-30 13:12:45 +0500

    bbb

--- src/Componrnts/Form.jsx
+++ src/Componrnts/Form.jsx
@@ -2,20 +2,26 @@ import React, { useState } from 'react'
 
 function Form() {
     const [value, setValue] = useState('')
+    const[title, setTitle] = useState('')
 
     const handleChenge = (e) => {
         setValue(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setTitle(value)
+        setValue('')
     }
     
 
 
     return(
         <div>
-            <h1>{}</h1>
+            <h2>{title}</h2>
             <form action="">
             <input value={value} onChange={(handleChenge) } type="text" placeholder="Your" />
             </form>
-            <button onClick={(handleChenge)}>Enter</button>
+            <button onClick={handleSubmit}>Enter</button>
             
         </div>
     )
